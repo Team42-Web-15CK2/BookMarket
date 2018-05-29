db0a7be012e279d07bd623fdad7ba5b1f81e20ca
khiêm võ <volebaokhiem1711@gmail.com>
2018-05-29 20:42:31 +0700

    asdsad

---
+++ migrations/20180527104813-create-comment-table.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('comment-tables', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      id_user: {
+        type: Sequelize.INTEGER
+      },
+      id_product: {
+        type: Sequelize.INTEGER
+      },
+      content: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('comment-tables');
+  }
+};
